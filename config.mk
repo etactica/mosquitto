mqttz/mqttz

commit 433ee5c4d6852b507b43eae9c9a2c9028c6b48e5

[344] Don't compile in async dns support by default for makefiles.

config.mk
@@ -76,6 +76,9 @@ WITH_DOCS:=yes
 # Build with client support for SOCK5 proxy.
 WITH_SOCKS:=yes
 
+# Build with async dns lookup support for bridges (temporary). Requires glibc.
+#WITH_ADNS:=yes
+
 # =============================================================================
 # End of user configuration
 # =============================================================================
@@ -237,6 +240,11 @@ endif
 
 ifeq ($(WITH_EC),yes)
 	BROKER_CFLAGS:=$(BROKER_CFLAGS) -DWITH_EC
+endif
+
+ifeq ($(WITH_ADNS),yes)
+	BROKER_LIBS:=$(BROKER_LIBS) -lanl
+	BROKER_CFLAGS:=$(BROKER_CFLAGS) -DWITH_ADNS
 endif
 
 MAKE_ALL:=mosquitto

lib/mosquitto_internal.h
@@ -155,7 +155,7 @@ struct mosquitto {
 #ifndef WITH_BROKER
 	mosq_sock_t sockpairR, sockpairW;
 #endif
-#ifdef __GLIBC__
+#if defined(__GLIBC__) && defined(WITH_ADNS)
 	struct gaicb *adns; /* For getaddrinfo_a */
 #endif
 	enum _mosquitto_protocol protocol;

lib/net_mosq.c
@@ -271,7 +271,7 @@ static unsigned int psk_client_callback(SSL *ssl, const char *hint,
 }
 #endif
 
-#if defined(WITH_BROKER) && defined(__GLIBC__)
+#if defined(WITH_BROKER) && defined(__GLIBC__) && defined(WITH_ADNS)
 /* Async connect, part 1 (dns lookup) */
 int _mosquitto_try_connect_step1(struct mosquitto *mosq, const char *host)
 {

src/bridge.c
@@ -131,7 +131,7 @@ int mqtt3_bridge_new(struct mosquitto_db *db, struct _mqtt3_bridge *bridge)
 		return MOSQ_ERR_NOMEM;
 	}
 
-#ifdef __GLIBC__
+#if defined(__GLIBC__) && defined(WITH_ADNS)
 	new_context->bridge->restart_t = 1; /* force quick restart of bridge */
 	return mqtt3_bridge_connect_step1(db, new_context);
 #else

src/loop.c
@@ -247,7 +247,7 @@ int mosquitto_main_loop(struct mosquitto_db *db, mosq_sock_t *listensock, int li
 					if((context->bridge->start_type == bst_lazy && context->bridge->lazy_reconnect)
 							|| (context->bridge->start_type == bst_automatic && now > context->bridge->restart_t)){
 
-#ifdef __GLIBC__
+#if defined(__GLIBC__) && defined(WITH_ADNS)
 						if(context->adns){
 							/* Waiting on DNS lookup */
 							rc = gai_error(context->adns);

etactica/mosquitto

commit 3beccb6fa34e51245f9533e91b8bc9164435cd50

[344] Only link libanl when WITH_ADNS=yes
Removes the hard link of libanl for Linux, leaving the protected link
when WITH_ADNS is specified.

Fixes: 433ee5c4d6852b507b43eae9c9a2c9028c6b48e5

Signed-off-by: Karl Palsson <karlp@etactica.com>

config.mk
@@ -159,10 +159,6 @@ ifeq ($(UNAME),QNX)
 	LIB_LIBS:=$(LIB_LIBS) -lsocket
 endif
 
-ifeq ($(UNAME),Linux)
-	BROKER_LIBS:=$(BROKER_LIBS) -lanl
-endif
-
 ifeq ($(WITH_WRAP),yes)
 	BROKER_LIBS:=$(BROKER_LIBS) -lwrap
 	BROKER_CFLAGS:=$(BROKER_CFLAGS) -DWITH_WRAP